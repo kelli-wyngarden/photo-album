a161fd40a7d1746d44a8bcd6ce81a9d7788bf5c0
kelli-wyngarden <kelli.r.wood@gmail.com>
2023-07-06 11:39:04 -0500

    Changed prompt from print to input and added try/except around casting input to int

--- album/app.py
+++ album/app.py
@@ -1,3 +1,6 @@
 def prompt_for_album():
-    print("Please enter the ID of the photo album you would like to view content for: ")
-    return
+    try:
+        album = int(input("Please enter the ID of the photo album you would like to view content for: "))
+        return album
+    except Exception:
+        print("Invalid input. Please try again.")
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,16 @@ def test_prompt_for_album_prints_prompt(capsys):
     app.prompt_for_album()
     prompt = capsys.readouterr()
     assert "Please enter the ID of the photo album you would like to view content for: " in prompt.out
+
+
+def test_prompt_for_album_prints_error_if_input_is_string(capsys, monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda x: "three")
+    app.prompt_for_album()
+    prompt = capsys.readouterr()
+    assert "Invalid input. Please try again." in prompt.out
+
+
+def test_prompt_for_album_returns_value_if_input_is_int(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda x: "3")
+    result = app.prompt_for_album()
+    assert result == 3
