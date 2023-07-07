9cbc601e4f701acc545291bd86f91644d9ee9898
kelli-wyngarden <kelli.r.wood@gmail.com>
2023-07-06 20:58:41 -0500

    Added print statements for returned photo album and photos

--- album/app.py
+++ album/app.py
@@ -5,4 +5,7 @@ def run_app():
     album = None
     while album is None:
         album = service.prompt_for_album()
-    service.request_photos(album)
+    photos = service.request_photos(album)
+    print(f"\nPhoto album {album} contains the following photos:")
+    for photo in photos:
+        print(f"\t{photo['id']}: {photo['title']}")
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,8 +12,19 @@ def test_app_calls_prompt_for_album_until_return_value_not_none(monkeypatch):
 
 def test_app_calls_request_photos_with_album_id(monkeypatch):
     expected_album_id = 4
-    mock_request_photos = Mock()
+    mock_request_photos = Mock(return_value=[])
     monkeypatch.setattr("album.service.request_photos", mock_request_photos)
     monkeypatch.setattr("album.service.prompt_for_album", lambda: expected_album_id)
     run_app()
     mock_request_photos.assert_called_once_with(expected_album_id)
+
+
+def test_app_prints_photos_returned_from_request_photos(monkeypatch, capsys):
+    photos = [{"id": "1", "title": "photo1"}, {"id": "2", "title": "photo2"}]
+    monkeypatch.setattr("album.service.request_photos", lambda x: photos)
+    monkeypatch.setattr("album.service.prompt_for_album", lambda: 56)
+    run_app()
+    prompt = capsys.readouterr()
+    assert "Photo album 56 contains the following photos:" in prompt.out
+    assert "1: photo1" in prompt.out
+    assert "2: photo2" in prompt.out
